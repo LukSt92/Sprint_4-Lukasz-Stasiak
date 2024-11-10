9c7c35bc6448eaa4b110b001d32466b555f36b73
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-11-10 15:41:10 +0100

    add: editTask function

--- script.js
+++ script.js
@@ -6,17 +6,45 @@ function addTask() {
   else {
     const listToAdd = document.getElementById("toDoList");
     const newTask = document.createElement("li");
+    const newTaskName = document.createElement("span");
     const newEditBtn = document.createElement("button");
     newEditBtn.setAttribute("class", "editBtn");
     newEditBtn.innerText = "Edytuj";
+    newEditBtn.addEventListener("click", editTask);
     const newDeleteBtn = document.createElement("button");
     newDeleteBtn.setAttribute("class", "deleteBtn");
     newDeleteBtn.innerText = "Usuń";
-    newTask.innerText = mainInput.value;
+    newTaskName.innerText = mainInput.value;
     listToAdd.appendChild(newTask);
+    newTask.appendChild(newTaskName);
     newTask.appendChild(newEditBtn);
     newTask.appendChild(newDeleteBtn);
     mainInput.value = "";
   }
+}
+function editTask() {
+  this.removeEventListener("click", editTask);
+  const taskToEdit = this.parentNode;
+  let nameToEdit = taskToEdit.firstChild.innerText;
+  const editInput = document.createElement("input");
+  editInput.setAttribute("id", "temporaryInput");
+  editInput.value = nameToEdit;
+  taskToEdit.insertBefore(editInput, this);
+  const toRemove = document.getElementById("temporaryInput");
+  taskToEdit.firstChild.innerText = "";
+  this.innerText = "Zatwierdź zmiany";
+  this.addEventListener("click", function saveChanges() {
+    const validCheck = editInput.value.length > 0;
+    console.log(validCheck);
+    if (!validCheck) {
+      alert("Nazwa zadania nie może być pusta.");
+    } else {
+      taskToEdit.firstChild.innerText = editInput.value;
+      toRemove.remove();
+      this.removeEventListener("click", saveChanges);
+      this.innerText = "Edytuj";
+      this.addEventListener("click", editTask);
+    }
+  });
 }
 addTaskBtn.addEventListener("click", addTask);
